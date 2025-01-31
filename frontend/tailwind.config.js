6c6cb1d7e35f73a279b38dbac261d0d61c540e81
Anthony Hernandez <jrdaiv@icloud.com>
2025-01-30 18:04:27 -0800

    maybe now? maybe

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}" // ✅ Ensure correct paths
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
